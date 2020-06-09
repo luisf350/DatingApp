6b7384c909273d648624b957ed6223cb03c2e53a
Luis Fernando Ramirez <lramirez@webcreek.com>
2020-06-09 16:22:36 -0500

    Add Unit Test project

---
+++ DatingApp.Test/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using DatingApp.API;
+using DatingApp.API.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatingApp.Test.Controllers
+{
+    [TestClass]
+    public class WeatherForecastControllerTest
+    {
+        [TestMethod]
+        public void GetTest()
+        {
+            var controller = new WeatherForecastController(null);
+
+            var result = controller.Get() as WeatherForecast[];
+            Assert.AreEqual(5, result.Length);
+        }
+    }
+}
